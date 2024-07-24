b7bed5cfdd685757029ac3abf2d121b8b4ced5b2
Ranjana0603 <113023714+Ranjana0603@users.noreply.github.com>
2024-07-24 14:13:12 +0530

    Add files via upload

---
+++ Traning.py
@@ -0,0 +1,120 @@
+import tensorflow as tf 
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+# Opening the files about data
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+# normalizing data (a pixel goes from 0 to 255)
+X = X/255.0
+
+# Building the model
+model = Sequential()
+# 3 convolutional layers
+model.add(Conv2D(32, (3, 3), input_shape = X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+# 2 hidden layers
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+model.add(Dense(128))
+model.add(Activation("relu"))
+
+# The output layer with 13 neurons, for 13 classes
+model.add(Dense(3))
+model.add(Activation("softmax"))
+
+# Compiling the model using some basic parameters
+model.compile(loss="sparse_categorical_crossentropy",
+				optimizer="adam",
+				metrics=["accuracy"])
+y=np.array(y)
+# Training the model, with 40 iterations
+# validation_split corresponds to the percentage of images used for the validation phase compared to all the images
+history = model.fit(X, y, batch_size=32, epochs=25, validation_split=0.1)
+
+
+
+model.save('CNN.model')
+
+# Printing a graph showing the accuracy changes during the training phase
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+acc=np.array(acc)
+val_acc=np.array(val_acc)
+loss=np.array(loss)
+val_loss=np.array(val_loss)
+epochs_range = range(25)
+
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+
+
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+vgg_y_pred =  model.predict_generator(X)
+y_pred_array=np.array(vgg_y_pred)
+#vgg_y_p = np.where(vgg_y_pred > 0.5, 1,0)
+#test_data=dataset_test.unbatch()
+y_g=[]
+#for image, label in  test_data:
+#  y_g.append(label.numpy())
+# compute the confusion matrix
+
+print(y)
+print(y_pred_array)
+yt=[]
+for xt in y_pred_array:
+  yt.append(xt.tolist().index(max(xt)))
+print(yt)
+
+
+from sklearn.metrics import classification_report
+
+print('\nClassification Report\n')
+print(classification_report(y, yt, target_names=['Class 1', 'Class 2','Class 3']))
+
+
+
+
+confusion_mtx = confusion_matrix(y, yt) 
+# plot the confusion matrix
+f,ax = plt.subplots(figsize=(8, 8))
+sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,cmap="Blues",linecolor="gray", fmt= '.1f',ax=ax)
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix")
+plt.show()
+
